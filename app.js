ff32544ef7c37628b0d3eeb00298759790cc8ae9
IsaacLezama <isaaclez19@gmail.com>
2022-09-27 23:52:19 -0500

    using module file system and read file

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 const http = require('http');
+const fs = require('fs');
 
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
 
     if(req.url === "/") {
-        
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('page/home.html');
-        res.end();
-    }
-
-    if(req.url === "/about") {
-        
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({
-        data: 'Now this is about page'
-      }));
-    } else{
-        res.writeHead(404, { 'Content-Type': 'application/json' });
-        res.write('Page not found');
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      fs.readFile("page/home.html", "utf8", (err, data) => {
+        // console.log(data);
+          if(err) throw err;
+          res.write(data);
+          res.end();
+        });
+    } else if(req.url === "/about") {
+      fs.readFile("page/about.html", "utf8", (err, data) => {
+        // console.log(data);
+          if(err) throw err;
+          res.write(data);
+          res.end();
+        });
+    } else {
+      fs.readFile("page/404.html", "utf8", (err, data) => {
+        // console.log(data);
+          if(err) throw err;
+          res.write(data);
+          res.end();
+        });
     }
 });
 
