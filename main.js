716b0c399b893c6394e81185de2ec7fd1f16f2df
IsaacLezama <isaaclez19@gmail.com>
2022-09-29 17:29:21 -0500

    understanding node event emitter

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
+// const EventEmitter = require('events');
+// const myEvent = new EventEmitter();
+
+// myEvent.on('test-event', (data) => {
+//     console.log('this event is listening');
+//     console.log(data);
+// });
+
 const express = require('express');
 const app = express();
 const PORT = 3001;
 
 app.get('/', (req, res) => {
+    // myEvent.emit("test-event", {name: `Isaac`});
     res.send('Hello world');
 });
 
