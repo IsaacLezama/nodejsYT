4e42f8fa20ad1e5969b99cfe075275127c6cb850
IsaacLezama <isaaclez19@gmail.com>
2022-10-10 17:31:53 -0500

    expressjs server html file

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const myBookRouter = require("./book");
+const path = require("path");
 
 router.get('/', (req, res) => {
     // myEvent.emit("test-event", {name: `Isaac`});
-    res.send('Hello world');
+    res.sendFile( path.join(__dirname, '/../page/home.html'), {name: 'Isaac'});
 });
 
 router.use('/book', myBookRouter);
 
 router.all('/*', (req, res) => {
-    res.send("Page not found");
+    // res.send("Page not found");
+    res.sendFile( path.join(__dirname, '/../page/404.html'));
 });
 
 module.exports = router;
